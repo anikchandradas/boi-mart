ef50078de72cd4378308dc82bfcd1ca9903326de
anik das <anikchandradas.freelancer@gmail.com>
2020-03-12 09:39:09 +0600

    Date: 12-03-2020; create header area

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bio-mart</title>
+    <link rel="stylesheet" href="css/icon.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="shortcut icon" type="image/png" href="#">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="images/logo.png">
+        </div>
+        <div class="search_bar">
+            <input type="text" name="name" placeholder="search in bio-mart">
+            <button class="search_button">
+                <i class="material-icons">search</i>
+            </button>
+        </div>
+        <div class="icons">
+            <a href="#" class="">
+                <i class="material-icons">shopping_cart</i>
+            </a>
+            <a href="#" class="">
+                <i class="material-icons">phone</i>
+                <span>01521569529</span>
+            </a>
+        </div>
+    </header>
+    <div class="content">
+        contain area
+    </div>
+    <footer>
+        footer area
+    </footer>
+</body>
+</html>
